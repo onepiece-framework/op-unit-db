b6e4642e6b01fc33bcd0e1253789ce0853e39abe
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-11-28 21:08:24 +0900

    New: DB.class.php

--- DB.class.php
+++ DB.class.php
@@ -20,8 +20,36 @@
  */
 class db extends OnePiece
 {
-	function Connect()
+	/**
+	 * PDO instance handle.
+	 *
+	 * @var PDOStatement
+	 */
+	private $_dbh;
+
+	/**
+	 * Database connection.
+	 */
+	function Connect($args)
 	{
-		return true;
+		//	...
+		foreach(['driver','host','database','user','password','charset'] as $key){
+			if( null === ${$key} = $args[$key]){
+				Notice::Set("Has not been set this key's value. ($key)");
+			}
+		}
+
+		//	...
+		$dsn	 = "{$driver}:host={$host};dbname={$database}";
+		$options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES '{$charset}'";
+
+		//	...
+		try{
+			$this->_dbh = new PDO($dsn, $user, $password, $options);
+		}catch(PDOException $e){
+			Notice::Set($e->getMessage());
+		}
+
+		return $this->_dbh ? true: false;
 	}
 }
