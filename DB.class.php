7cd046ab75c1f2c81ead9ca05b26b65c1600d8ac
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-11-28 14:58:23 +0900

    New: index.php

---
+++ DB.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * unit-db:/DB.class.php
+ *
+ * @created   2016-11-28
+ * @version   1.0
+ * @package   unit-db
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * db
+ *
+ * @created   2016-11-28
+ * @version   1.0
+ * @package   unit-db
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class db extends OnePiece
+{
+	function Connect()
+	{
+		return true;
+	}
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * unit-db:/index.php
+ *
+ * @created   2016-11-28
+ * @version   1.0
+ * @package   unit-db
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+include(__DIR__.'/DB.class.php');
